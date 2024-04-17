677b9cc35459a05361362e44d489e526de3ec569
ChadmiRatnayake <chadmi.20@cse.mrt.ac.lk>
2024-04-17 15:31:10 +0530

    Insert data at start-up

--- src/main/java/com/example/reactiveDemo/ReactiveDemoApplication.java
+++ src/main/java/com/example/reactiveDemo/ReactiveDemoApplication.java
@@ -1,13 +1,34 @@
 package com.example.reactiveDemo;
 
+import com.example.reactiveDemo.student.Student;
+import com.example.reactiveDemo.student.StudentService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ReactiveDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveDemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			StudentService service
+	){
+		return args -> {
+			for (int i=0; i<100; i++){
+				service.save(
+						Student.builder()
+								.firstName("Kiwi " + i)
+								.lastName("Teddy " + i)
+								.age(i)
+								.build()
+				);
+			}
+		};
 	}
 
 }
